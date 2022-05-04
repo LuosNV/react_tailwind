aa2ef5064d2eff6746f96c478540821b199db441
Luis <luis.henriqe17@gmail.com>
2022-05-04 12:24:43 -0300

    kasdka

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export const App = () => {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen bg-violet-10">
+      <h1 className="text-violet-4">Main</h1>
+    </div>
+  );
+}
